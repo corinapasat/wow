6e169b38fb5cdcb7654f6a27bc7c38d4d0d491a0
corinapasat <115903046+corinapasat@users.noreply.github.com>
2024-06-10 17:20:06 +0300

    GestionareParcAuto

---
+++ GestionareParcAuto/Vehicul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionareParcAuto
+{
+    public abstract class Vehicul
+    {
+        private int id;
+        private string proprietar;
+        private float pret;
+
+        public int Id { get => id; set => id = value; }
+        public string Proprietar { get => proprietar; set => proprietar = value; }
+        public float Pret { get => pret; set => pret = value; }
+
+        protected Vehicul(int id, string proprietar, float pret)
+        {
+            this.id = id;
+            this.proprietar = proprietar;
+            this.pret = pret;
+        }
+    }
+}
