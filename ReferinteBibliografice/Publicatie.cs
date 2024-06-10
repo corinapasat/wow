95c13f18c0924991695d1ebba933cd1f2c93a749
corinapasat <115903046+corinapasat@users.noreply.github.com>
2024-06-10 17:25:59 +0300

    ReferinteBibliografice

---
+++ ReferinteBibliografice/Publicatie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferinteBibliografice
+{
+    public abstract class Publicatie
+    {
+        public string titlu;
+        public float pret;
+
+        public Publicatie(string titlu, float pret)
+        {
+            this.titlu = titlu;
+            this.pret = pret;
+        }
+
+        public Publicatie()
+        {
+
+        }
+        
+
+        public string Titlu { get => titlu; set => titlu = value; }
+        public float Pret { get => pret; set => pret = value; }
+
+        
+
+        public abstract string genereazaReferinta();
+    }
+}
